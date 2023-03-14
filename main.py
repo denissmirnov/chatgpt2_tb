a864bc43353c2a1bab2dc5cf8b5be646f92e72aa
Denis Smirnov <denis.smirnov@inplat.ru>
2023-03-14 17:52:50 +0300

    some work

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import uuid
 
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 from aiogram.types import ParseMode
 from loguru import logger as LOGGER
@@ -16,61 +17,62 @@ gpt_node_url = os.environ.get('CHATGPT_TB_GPT_NODE_URL', 'http://127.0.0.1:8000'
 gpt = GptApiClient(gpt_node_url)
 bot = Bot(token=os.environ.get('API_TOKEN'))
 dp = Dispatcher(bot)
-chats: list = []
-conversations: dict = {}
+
+class Chat(BaseModel):
+    chat_id: int
+    conversation_id: uuid.UUID
+
+class Conversation(BaseModel):
+    query: str
+    answer: str
+    parent_id: uuid.UUID
+    account_id: uuid.UUID
+
+chats: list[Chat] = []
+conversations: dict[uuid.UUID, list[Conversation]] = {}
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO)
 
-
 setup_logging()
-
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     conversation_id = uuid.uuid4()
-    chats.append({f"{message.chat.id}": str(conversation_id)})
-    conversations[str(conversation_id)] = []
+    chat = Chat(chat_id=message.chat.id, conversation_id=conversation_id)
+    chats.append(chat)
+    conversations[conversation_id] = []
     await message.reply("Hi!\nI'm ChatGPT bot!\nAsk me something through /ask")
-
 
 @dp.message_handler(commands=['ask'])
 async def ask_gpt(message: types.Message):
     conversation_id, account_id = None, None
     for chat in chats:
-        if str(message.chat.id) in chat:
-            conversation_id = str(chat[str(message.chat.id)])
+        if chat.chat_id == message.chat.id:
+            conversation_id = chat.conversation_id
 
-    conversation: list = conversations[str(conversation_id)]
+    conversation: list[Conversation] = conversations.get(conversation_id, [])
     if len(conversation) > 0:
         msg = conversation[-1]
-        parent_id = msg.get('parent_id', uuid.uuid4())
-        account_id = msg.get('account_id')
+        parent_id = msg.parent_id
+        account_id = msg.account_id
     else:
         parent_id = uuid.uuid4()
         account_id = None
 
     query = message.text.replace('/ask ', '')
     response = await gpt.ask(query, conversation_id, parent_id, account_id)
-    print(response)
 
     if response and response.message:
-        msg = {
-            'query': query,
-            'answer': response.message,
-            'parent_id': f"{response.parent_id}",
-            'account_id': response.account_id,
-        }
+        msg = Conversation(query=query, answer=response.message, parent_id=response.parent_id, account_id=response.account_id)
 
-        conversations[str(conversation_id)].append(msg)
+        conversations[conversation_id].append(msg)
 
         LOGGER.info("Response was received")
-        print(conversations)
         await message.reply(response.message, parse_mode=ParseMode.MARKDOWN)
     else:
         LOGGER.error(f"Exception during parsing response. res: {response}")
         await message.reply("Some exception happened during parsing response. Please re-ask your question.")
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
